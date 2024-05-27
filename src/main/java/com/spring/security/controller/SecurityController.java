b27b9c4b8572f96c2068426fe2b7efc4264e3667
GanesanGhy01 <169020009+GanesanGhy01@users.noreply.github.com>
2024-05-27 18:19:18 +0530

    completed JWT Token concepts

--- src/main/java/com/spring/security/controller/SecurityController.java
+++ src/main/java/com/spring/security/controller/SecurityController.java
@@ -1,12 +1,16 @@
 package com.spring.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.security.entity.User;
+import com.spring.security.service.JwtService;
 import com.spring.security.service.RegisterService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +21,12 @@ public class SecurityController {
 	
 	@Autowired
 	RegisterService service;
+	
+	@Autowired
+	JwtService jwtService;
+	
+	@Autowired
+	AuthenticationManager mAuthenticationManager;
 	
 	@GetMapping("hello")
 	public String greet(HttpServletRequest request) {
@@ -31,5 +41,17 @@ public class SecurityController {
 	@PostMapping("register")
 	public User save(@RequestBody User user) {
 		return service.login(user);
+	}
+	
+	@PostMapping("login")
+	public String login(@RequestBody User user) {
+		Authentication authentication = mAuthenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		if(authentication.isAuthenticated()) {
+			return jwtService.generateToken(user.getUsername());
+		}else {
+			return "fail";
+		}
+		
 	}
 }
